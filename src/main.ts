6fe3c6b9d23691dfd0e9f5ec36dd133f0df29446
Lars Schönemann <lars.dev90@gmail.vom>
2026-06-11 01:49:15 +0200

    refactory scss and root

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
-import "./styles/style.scss"
+import "./main.scss"
 
-init()
+
+init(
+
+)
 
 function init() {
-    const fieldRef = document.getElementById("field")
-    if (fieldRef) {
-        fieldRef.addEventListener("click", e => {
-            const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement
-            if (card) {
-                card.classList.toggle("is-flipped")                
-            }
-        })
+layoutChange()
+}
+
+// function init() {
+//     const fieldRef = document.getElementById("field")
+//     if (fieldRef) {
+//         fieldRef.addEventListener("click", e => {
+//             const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement
+//             if (card) {
+//                 card.classList.toggle("is-flipped")
+//             }
+//         })
+//     }
+// }
+
+function layoutChange() {
+    const img = document.getElementById("theme-img")
+    if (img) {
+        img.src = "assets/img/blue.png"
+
     }
 }
