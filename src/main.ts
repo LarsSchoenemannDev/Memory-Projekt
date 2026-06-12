175f4c5c5f344de6aaa0788ccbdaccb649bb00fb
Lars Schönemann <lars.dev90@gmail.vom>
2026-06-12 14:53:09 +0200

    add game themes change and angry ts

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,36 @@
 import "./main.scss"
 
-interface gameSettings {
-    img: [];
+interface GameSettings {
+    theme: string[];
     player: string[];
     mapSize: string[];
 }
 
-const gameSettings = {
+const gameSettings: GameSettings = {
     theme: [],
     player: [],
     mapSize: []
 };
 
-init(
-
-)
+init()
 
 function init() {
     layoutChange()
     changeImg()
 }
 
-// function init() {
-//     const fieldRef = document.getElementById("field")
-//     if (fieldRef) {
-//         fieldRef.addEventListener("click", e => {
-//             const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement
-//             if (card) {
-//                 card.classList.toggle("is-flipped")
-//             }
-//         })
-//     }
-// }
+function clearform() {
+    let x = Object.values(gameSettings)
+    x.forEach(e => e.length = 0)
+}
 
 function layoutChange() {
+    clearform()
     const checked = Array.from(document.querySelectorAll<HTMLElement>(":checked")); // daten hollen 
     if (checked.length > 0) {
         checked.forEach(e => {
             const data = e.dataset
-            if (e.dataset.theme) {
+            if (data.theme) {
                 gameSettings.theme.push(data.theme);
             }
             if (data.playerselect) {
@@ -48,14 +40,25 @@ function layoutChange() {
                 gameSettings.mapSize.push(data.size);
             }
         })
-    }    
+    }
 }
 
 function changeImg() {
-    const img = document.querySelectorAll<HTMLElement>("#theme-img");
-    let x = img
-    x.include(scr)
-    console.log(x.length);
+    if (gameSettings.theme.length === 0) {
+        return
+    } else {
+        const img = document.querySelectorAll(".wrapper__img img");
+        img.forEach(e => {
+            const isMatch: boolean = e.classList.contains(gameSettings.theme[0] || "");
+            if (isMatch) {
+                e.classList.remove("hidden");
+            } else {
+                e.classList.add("hidden");
+            }
+        });
+    }
 }
 
-// daten hollen am besten alle thems dann mit forech if abgleich frage und das bild schalten dann einfach am besten per onclick interhängen
+document.addEventListener("change", init)
+
+
